ac2675d397f075a2d727a5b55cb01c66e833f2d4
Felix MIL <felix.mil@esqlabs.com>
2024-07-03 11:18:04 +0200

    add a dummy function

---
+++ R/my_function.R
@@ -0,0 +1,15 @@
+#' Dummy Function
+#'
+#' @description adds 1 to the input
+#'
+#' @param x numeric input
+#'
+#' @return numeric output
+#' @export
+#'
+#' @examples
+#' my_function(1)
+#' my_function(10)
+dummy_fun <- function(x) {
+  x + 1
+}
---
+++ tests/testthat/test-dummy_fun.R
@@ -0,0 +1,5 @@
+test_that("my_function works", {
+  expect_equal(dummy_fun(1), 2)
+
+  expect_equal(dummy_fun(10), 11)
+})
