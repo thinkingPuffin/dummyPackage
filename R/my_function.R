35bfa9452c2bb45d6baa4dd5535b21b2ace70e68
Felix MIL <dev@felixmil.com>
2025-02-11 11:11:26 +0100

    Version bump: add a file

--- R/my_function.R
+++ R/my_function.R
@@ -14,15 +14,3 @@ dummy_fun <- function(x) {
   x + 1
 }
 
-#' Another Function
-#'
-#' @param x  numeric input
-#'
-#' @return numeric output
-#' @export
-#'
-#' @examples
-#' another_fun(1)
-another_fun <- function(x) {
-  x + 2
-}
---
+++ R/my_other_function.R
@@ -0,0 +1,12 @@
+#' Another Function
+#'
+#' @param x  numeric input
+#'
+#' @return numeric output
+#' @export
+#'
+#' @examples
+#' another_fun(1)
+another_fun <- function(x) {
+  x + 2
+}
