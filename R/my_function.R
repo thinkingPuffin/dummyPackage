65aadd5c48557594596b0506ca753a265da11f55
Felix MIL <dev@felixmil.com>
2025-02-11 10:57:48 +0100

    This commit should trigger version bump

--- R/my_function.R
+++ R/my_function.R
@@ -13,3 +13,16 @@
 dummy_fun <- function(x) {
   x + 1
 }
+
+#' Another Function
+#'
+#' @param x  numeric input
+#'
+#' @return numeric output
+#' @export
+#'
+#' @examples
+#' another_fun(1)
+another_fun <- function(x) {
+  x + 2
+}
